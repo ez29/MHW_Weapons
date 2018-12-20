6c4d1b9e94e10e5f06f941a6db456c5ec0783251
Edwin <ez29@njit.edu>
2018-12-20 16:44:08 -0500

    Update web.config and connection string

--- MHW_Weapons/Default.aspx.cs
+++ MHW_Weapons/Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,8 +13,28 @@ namespace MHW_Weapons
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string strConnString = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString;
 
+           // SqlCommand command = new SqlCommand("select * from Users");
+
+        }
+
+        protected SqlConnection GetSqlConnection()
+        {
+            try
+            {
+                //Using the Default Connection in the Web.Config File, establish an Oracle Connection
+                var conString = System.Configuration.ConfigurationManager.ConnectionStrings["SQLConnection"];
+                string strConnString = conString.ConnectionString;
+                return new SqlConnection(strConnString);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("SQL Exception Message");
+                Console.WriteLine("Exception Message: " + ex.Message);
+                Console.WriteLine("Exception Source: " + ex.Source);
+                return null;
+            }
         }
     }
 }
